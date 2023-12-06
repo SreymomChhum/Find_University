ba699ebdfde1991d77af40931dbb4c62d2a43bf0
SreymomChhum <momkpc310@gmail.com>
2023-12-06 01:59:45 -0500

    add project

---
+++ backend/resources/views/userEmail.blade.php
@@ -0,0 +1,4 @@
+<fieldset>
+    <h1>Click for reset new password!</h1>
+    <a href="http://localhost:8080/resetPassword">Click me</a>
+</fieldset>
---
+++ frontend/src/components/about/OurSchool/OurSchoolVision.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="mision">
+    <div class="text">
+      <div class="missiontext">
+        <h2>VALUE</h2>
+        <h4>
+          Trust <br />
+          Responsibility <br />
+          Solidarity <br />
+          Respect <br>
+          Demanding approach
+        </h4>
+      </div>
+    </div>
+    <div class="image">
+      <img
+        src="../../../assets/images/pn2.jpg"
+        alt=""
+        style="width: 600px; height: 400px"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.mision {
+  display: flex;
+  flex-direction: row-reverse;
+  width: 95%;
+  margin: auto;
+}
+.text {
+  margin: auto;
+  text-align: center;
+  width: 60%;
+}
+h2{
+  color: orange;
+   margin-bottom: 15px;
+
+}
+</style>
